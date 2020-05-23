aaa8ba49c0a0eaefa7b5b7d054d89a9e2209eefc
Felipe Carvalho <feliped3carvalho@gmail.com>
2020-05-23 01:43:34 -0300

    criado modulo home,app

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_go_fast/app/app_module.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 void main() {
-  runApp(ModularApp(module:  AppModule(),));
+  runApp(ModularApp(
+    module: AppModule(),
+  ));
 }
-
-/*class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Gofast',
-      theme: ThemeData(
-        primarySwatch: Colors.orange,
-       
-      ),
-      home: MyHomePage(),
-    );
-  }
-}*/
